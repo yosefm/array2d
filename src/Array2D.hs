49f4da04cbb68796d53a405afafa615374fd0ea6
Yosef Meller <yosefm@gmail.com>
2025-04-09 19:56:23 +0300

    Change Array2D backend to vector

--- src/Array2D.hs
+++ src/Array2D.hs
@@ -5,7 +5,10 @@ module Array2D (
   (@), asRows, 
   merge)
   where 
- 
+
+import qualified Data.Vector as V
+import Data.Vector ((//), (!))
+
 import Data.List (sortBy)
 import Data.Function (on)
 
@@ -21,16 +24,16 @@ inBounds (Ex rows cols) = (&&)
 
 type Coords = (Int, Int)  -- row, col
 
-data Arr2D a = Arr2D  Extents [a]  deriving Show
+data Arr2D a = Arr2D  Extents (V.Vector a)  deriving Show
 
 mkArr2D :: Extents -> [a] -> Maybe (Arr2D a)
 mkArr2D e@(Ex rows cols) lst  
-  | length lst == rows * cols  = Just $ Arr2D e lst
+  | length lst == rows * cols  = Just $ Arr2D e $ V.fromList lst
   | otherwise                  = Nothing
 
   
 nullArr2D :: Arr2D a
-nullArr2D = Arr2D (Ex 0 0) []
+nullArr2D = Arr2D (Ex 0 0) V.empty
 
 flatIndex :: Extents -> Coords -> Int
 flatIndex (Ex _ cols) (row, col) = row*cols + col
@@ -39,42 +42,19 @@ fatIndex :: Extents -> Int -> Coords
 fatIndex (Ex _ cols) flat = divMod flat cols
 
 (@) :: Arr2D a -> Coords -> a
-Arr2D e lst @ c = lst !! flatIndex e c
+Arr2D e v @ c = v ! flatIndex e c
 
-asRows :: Arr2D a -> [[a]]
-asRows (Arr2D (Ex _ cols) lst) = go lst where 
-  go [] = []
-  go lst' =   
-    let (row, rest) = splitAt cols lst'
-    in row : go rest
-
--- assumes indices are within the length.
--- indices in replacement list start at 0.
-flatMergeSorted :: [a] -> Int -> [(Int, a)] -> [a]
-flatMergeSorted [] _ _ = []
-flatMergeSorted lst _ [] = lst
-flatMergeSorted lst count ((at, what) : rest) = 
-  let (h, t) = splitAt (at - count) lst
-  in h ++ (what : flatMergeSorted (tail t) (at + 1) rest)
+asRows :: Arr2D a -> [V.Vector a]
+asRows (Arr2D e@(Ex rows cols) v) = map (\n -> getRow n) [0..rows - 1]
+  where getRow n = V.slice (flatIndex e (n,0)) cols v
 
 type Repl a = (Int, a)
-
-ensureUnique :: [Repl a] -> Maybe [Repl a]
-ensureUnique lst = 
-  let srt = sortBy (compare `on` fst) lst
-      dummyElem = (-1, snd $ head srt)
-      cmpList = zip srt (dummyElem : srt)
-      maybeUniqueElem (n1, n2)
-        | fst n1 == fst n2 = Nothing
-        | otherwise = Just n1
-    
-  in traverse maybeUniqueElem cmpList 
-
 type Repl2D a = (Coords, a)
+
 toRepl1D :: Extents -> Repl2D a -> Repl a
 toRepl1D e src = ((flatIndex e . fst) src, snd src)
 
 merge :: Arr2D a -> [Repl2D a] -> Maybe (Arr2D a)
-merge (Arr2D e lst) repls = 
-  let flatReps = ensureUnique $ map (toRepl1D e) repls
-  in (Arr2D e . flatMergeSorted lst 0) <$> flatReps
+merge (Arr2D e v) repls = 
+  let flatReps = map (toRepl1D e) repls
+  in Just $ Arr2D e $ v // flatReps
